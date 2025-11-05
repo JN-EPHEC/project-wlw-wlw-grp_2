53a39e637b59a342e40bd83495f460f170995893
Hisokaplp16 <chayanplp16@gmail.com>
2025-11-05 12:29:21 +0100

    Test 1

---
+++ Firebase_env.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyACJHNHMMIp7sB2bx1QtWLPcH9QQqOAYv4",
+  authDomain: "swipeskills-cf784.firebaseapp.com",
+  projectId: "swipeskills-cf784",
+  storageBucket: "swipeskills-cf784.firebasestorage.app",
+  messagingSenderId: "30195503547",
+  appId: "1:30195503547:web:b2bdf5443bfac76f8c749e"
+};
