b77c0f129ed4bd3e9b19eade4a44d46819e23074
DylanNtumba <he123796@students.ephec.be>
2025-11-05 12:45:56 +0100

    ok

---
+++ components/Dylan.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const TextDylan = () => {
+  return (
+    <View>
+      <Text>20.000$ IN INVESTMENT !!!!!!!</Text>
+    </View>
+  );
+};
+
+export default TextDylan;
