5649aea8cc2afe36eec01e7a50ef6cf2527063a9
Oszust Kacper <oszust.kacper@gmail.com>
2025-11-12 12:13:17 +0100

    Fix env

--- firebase_env.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyACJHNHMMIp7sB2bx1QtWLPcH9QQqOAYv4",
-  authDomain: "swipeskills-cf784.firebaseapp.com",
-  projectId: "swipeskills-cf784",
-  storageBucket: "swipeskills-cf784.firebasestorage.app",
-  messagingSenderId: "30195503547",
-  appId: "1:30195503547:web:b2bdf5443bfac76f8c749e"
-};
-
