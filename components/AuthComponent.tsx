f115e8694a58a882cf3075c563892562da62aa17
Oszust Kacper <oszust.kacper@gmail.com>
2025-12-01 10:43:27 +0100

    firebase

--- components/AuthComponent.tsx
+++ components/AuthComponent.tsx
@@ -1,4 +1,4 @@
-import { router } from "expo-router";
+import { useRouter } from "expo-router";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
@@ -6,14 +6,23 @@ import {
   signOut,
   User,
 } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { Alert, Button, StyleSheet, Text, TextInput, View } from "react-native";
-import { auth } from "../firebaseConfig"; // Import de notre config
+import { auth, db } from "../firebaseConfig"; // Import de notre config
 
-const AuthComponent = () => {
+export default function AuthComponent(){
+const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user, setUser] = useState<User | null>(null); // Pour suivre l'utilisateur connecté
+   const [passwordControle, setPasswordControle] = useState("");
+  const [user, setUser] = useState<User | null>(null);
+  const [errorMessage, setErrorMessage] = useState ("");
+  const [loading, setLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+   const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordControleError, setPasswordControleError] = useState(false);
 
   // Écoute les changements d'état de l'authentification
   useEffect(() => {
@@ -48,17 +57,39 @@ const AuthComponent = () => {
 
   // Fonction de connexion
   const handleSignIn = async () => {
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log("Utilisateur connecté !", userCredential.user);
-    } catch (error: any) {
-      Alert.alert("Erreur Connexion", error.message);
+  setErrorMessage("");
+  setEmailError(false);
+  setPasswordError(false);
+
+  let hasError = false;
+  if (!email) {
+    setEmailError(true);
+    hasError = true;
+  }
+
+  if (hasError) return;
+
+  setLoading(true);
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const uid = userCredential.user.uid;
+
+    await setDoc(doc(db, "users", uid), {
+      email: userCredential.user.email,
+      createdAt: new Date(),
+      familyId: null,
+    }, { merge: true });
+
+  } catch (error: any) {
+    if (error.code == 'auth/invalid-email') {
+      setErrorMessage("Email ou mot de passe incorrect");
+    } else {
+      setErrorMessage("Une erreur est survenue. Veuillez réessayer.");
     }
-  };
+  } finally {
+    setLoading(false);
+  }
+};
 
   // Fonction de déconnexion
   const handleSignOut = async () => {
@@ -106,6 +137,7 @@ const AuthComponent = () => {
   );
 };
 
+
 // Styles (simplifiés)
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: "center", padding: 20 },
@@ -129,4 +161,3 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AuthComponent;
