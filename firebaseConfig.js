e5dbeb63d381966a5040a3857582edf71fb94b81
salmely <HE133520@students.ephec.be>
2025-12-08 17:54:29 +0100

    ok

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,23 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "TON_API_KEY",
+  apiKey: "AIzaSyACJHNHMMIp7sB2bx1QtWLPcH9QQqOAYv4",
   authDomain: "swipeskills-cf784.firebaseapp.com",
   projectId: "swipeskills-cf784",
   storageBucket: "swipeskills-cf784.firebasestorage.app",
-  messagingSenderId: "TON_MESSAGING_SENDER_ID",
-  appId: "TON_APP_ID"
+  messagingSenderId: "30195503547",
+  appId: "1:30195503547:web:8a06855de559e2b08c749e"
 };
 
-// Initialiser Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporter Authentication, Database ET Storage
+// Export Authentication, Firestore Database and Storage
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
