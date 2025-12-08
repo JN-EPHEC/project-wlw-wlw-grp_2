37b0c250d2a5c657da04c3302fabd7bad7014478
salmely <HE133520@students.ephec.be>
2025-12-08 17:50:21 +0100

    ok

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  // etc.
+  apiKey: "TON_API_KEY",
+  authDomain: "swipeskills-cf784.firebaseapp.com",
+  projectId: "swipeskills-cf784",
+  storageBucket: "swipeskills-cf784.firebasestorage.app",
+  messagingSenderId: "TON_MESSAGING_SENDER_ID",
+  appId: "TON_APP_ID"
 };
 
+// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporter les services
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
