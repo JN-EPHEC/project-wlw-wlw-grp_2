0657cc13d325816622d1bc52e7f3b8472bef9066
salmely <HE133520@students.ephec.be>
2025-12-08 17:18:19 +0100

    ok

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,18 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { firebaseConfig } from "./firebase_env";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "...",
+  authDomain: "...",
+  projectId: "...",
+  // etc.
+};
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
 export const auth = getAuth(app);
-export const storage = getStorage(app);
-export const db = getFirestore(app,"swipeskills");
+export const db = getFirestore(app);
